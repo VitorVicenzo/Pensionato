ed1756c800c04570d56fd3befb92ed8236813660
VitorVicenzo <vitorvicenzo70@gmail.com>
2026-03-29 15:43:59 -0300

    Finished.

--- pensao/src/main/java/pensionato/Main.java
+++ pensao/src/main/java/pensionato/Main.java
@@ -1,7 +1,34 @@
 package pensionato;
 
+import java.util.Scanner;
+
+import pensionato.Entities.Rooms;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+        Rooms[] vect_Room = new Rooms[10];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rent #" + (i+1) + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            vect_Room[room] = new Rooms(name, email, room);
+        }
+
+        System.out.println("Busy rooms:");
+        for (int i = 0; i < 10; i++) {
+            if (vect_Room[i] != null) {
+                System.out.println(vect_Room[i].getRoom() + ": " + vect_Room[i].getName() + ", " + vect_Room[i].getEmail());
+            }
+        }
     }
 }
